45251697466a3a1698f2e7cb3bcfb34a64a49b16
Sergio Andrés Herrera Vallejo <sergioandresherreravallejo@gmail.com>
2025-08-02 00:48:34 -0500

    Commit #6 - Carro ENSAMBLADO

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
- CHASIS = "En Progreso"
+from llantas import LLANTAS
+from vidrio import VIDRIO
+
+CHASIS = "CHASIS LISTO"
+
+res = LLANTAS[0] + LLANTAS[1] + LLANTAS[2] + LLANTAS[3] + CHASIS + VIDRIO
